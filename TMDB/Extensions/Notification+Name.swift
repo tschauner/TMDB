a19d656de29e07181b51ec24133072138ad02d54
Philipp Tschauner <philipp@Lissabon.fritz.box>
2019-11-03 15:14:15 +0100

    added extensions

---
+++ TMDB/Extensions/Notification+Name.swift
@@ -0,0 +1,21 @@
+//
+//  Notification+Name.swift
+//  MovieDatabase
+//
+//  Created by Philipp Tschauner on 02.11.19.
+//  Copyright © 2019 phitsch. All rights reserved.
+//
+
+import Foundation
+
+protocol NotificationName {
+    var name: Notification.Name { get }
+}
+
+extension RawRepresentable where RawValue == String, Self: NotificationName {
+    var name: Notification.Name {
+        get {
+            return Notification.Name(self.rawValue)
+        }
+    }
+}
---
+++ TMDB/Extensions/UIView+Nib.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Nib.swift
+//  MovieDatabase
+//
+//  Created by Philipp Tschauner on 02.11.19.
+//  Copyright © 2019 phitsch. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
