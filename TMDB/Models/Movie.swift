4b037b416b060ca78277ebf67d71acad811a071b
Philipp Tschauner <philipp@Lissabon.fritz.box>
2019-11-03 15:14:53 +0100

    added movie model

---
+++ TMDB/Models/Movie.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  MovieDatabase
+//
+//  Created by Philipp Tschauner on 02.11.19.
+//  Copyright © 2019 phitsch. All rights reserved.
+//
+
+import Foundation
+
+struct MovieResult: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let posterPath: String?
+    let genreIDS: [Int]?
+    let title: String?
+    let overview, releaseDate: String?
+    
+    var imageURL: URL? {
+        if let posterPath = posterPath {
+            return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath)")
+        } else {
+            return nil
+        }
+    }
+    
+    var releaseDataString: String? {
+        if let releaseDate = releaseDate {
+            return "Release \(releaseDate)"
+        } else {
+            return nil
+        }
+    }
+    
+    var genres: [String] {
+        return genreIDS?.compactMap { Genre(withId: $0)?.name } ?? []
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case genreIDS = "genre_ids"
+        case title
+        case overview
+        case releaseDate = "release_date"
+    }
+}
