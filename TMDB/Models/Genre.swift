c2dc3318a6430c68484668f27033a2d2b5d8c9e1
Philipp Tschauner <philipp@Lissabon.fritz.box>
2019-11-03 15:14:58 +0100

    added genre model

---
+++ TMDB/Models/Genre.swift
@@ -0,0 +1,30 @@
+//
+//  Genre.swift
+//  MovieDatabase
+//
+//  Created by Philipp Tschauner on 03.11.19.
+//  Copyright © 2019 phitsch. All rights reserved.
+//
+
+import Foundation
+
+struct GenreResult: Codable {
+    let genres: [Genre]
+}
+
+struct Genre: Codable {
+    let id: Int
+    let name: String
+}
+
+extension Genre {
+    init?(withId id: Int) {
+        let genre = MovieDataBase.shared.genres.first(where: { $0.id == id } )
+        if let genre = genre {
+            self.id = genre.id
+            self.name = genre.name
+        } else {
+            return nil
+        }
+    }
+}
